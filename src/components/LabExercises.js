4fb1596be6883fe2c0473f6c9227d972a0b2780e
Kubek-3 <jploskonka8@gmail.com>
2024-04-07 19:14:12 +0200

    small update in lab solutions

--- src/components/LabExercises.js
+++ src/components/LabExercises.js
@@ -1,6 +1,6 @@
 import { View, StyleSheet, Text, TextInput, FlatList, Image } from "react-native"
 import { useState } from "react"
-import {solution2data} from "../data/LabExercisesData"
+import { solution2data } from "../data/LabExercisesData"
 export const Container = ({ children }) => (
     <View style={styles.container}>
         <View style={styles.underline}></View>
@@ -9,33 +9,51 @@ export const Container = ({ children }) => (
     </View>
 )
 
-export const Solution1 = () =>
-{
-    const [name, setName] = useState("Bob")
+
+export const Solution1 = () => {
+    const [text, setText] = useState('Bob');
+    const [savedText, setSavedText] = useState('');
+
+    const handleTextChange = newText => {
+        setSavedText(newText);
+    };
+
+    const handleInputSubmit = () => {
+        setText(savedText);
+        setSavedText('');
+    };
+
     return (
-        <View style={{paddingVertical: 20 }}>
-            <Text style={{ marginBottom: 20, fontSize: 30 }}>Witaj {name}</Text>
-            <TextInput style={{ borderWidth: 1, borderRadius: 15, padding: 5, fontSize: 20, textAlign: "center" }}
+        <View style={styles.container}>
+            <View style={styles.underline}></View>
+            <Text style={styles.text}>Witaj {text}</Text>
+            <TextInput
+                value={savedText}
+                onChangeText={handleTextChange}
+                onSubmitEditing={handleInputSubmit}
                 placeholder="Wpisz imię"
-                onChangeText={setName}></TextInput>
+                style={[styles.input, { minWidth: 20, width: 'auto' }]} // ustawia mimalną szerokość, ale jeśli jest za dużo tekstu to poszerza się
+            />
+            <View style={styles.underline}></View>
         </View>
-    )
-}
+    );
+};
+
+export default Container;
 
 export const Solution2 = () => (
-    <View style={{flex:1, paddingVertical:10}}>
+    <View style={{ flex: 1, paddingVertical: 10 }}>
         <FlatList
             data={solution2data}
             showsHorizontalScrollIndicator={false}
             horizontal
             renderItem={
-                ({ item, index }) =>
-                {
+                ({ item, index }) => {
                     return (
                         <View key={item.id}
                             style={[styles.cardContainer,
-                            { marginRight: index === solution2data.length - 1 ? 10: 0}]}>
-                            <Image style={{width:50, height:50}} source={item.img} ></Image>
+                            { marginRight: index === solution2data.length - 1 ? 10 : 0 }]}>
+                            <Image style={{ width: 50, height: 50 }} source={item.img} ></Image>
                             <Text>{item.text}</Text>
                         </View>
                     )
@@ -44,8 +62,6 @@ export const Solution2 = () => (
         />
     </View>
 )
-
-
 
 
 const styles = StyleSheet.create({
@@ -67,8 +83,23 @@ const styles = StyleSheet.create({
         width: 100,
         justifyContent: "space-around",
         alignItems: "center",
-        backgroundColor:"white",
+        backgroundColor: "white",
         borderRadius: 10,
         elevation: 5,
-    }
+    },
+    text: {
+        fontSize: 24,
+        textAlign: 'center',
+        marginTop: 16,
+        underline: 'false'
+    },
+    input: {
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: 'grey',
+        padding: 10,
+        marginVertical: 20,
+        alignSelf: 'center',
+        borderRadius: 10,
+    },
 })
